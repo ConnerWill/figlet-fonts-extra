19d4e95229debd242e1d5381604cec54392a704b
ConnerWill <connerwill@connerwill.com>
2022-08-16 22:16:30 +0000

    cleaned up generate-plain.sh scripts and added script to print fonts in color using toilet

---
+++ generate-plain-COLOR.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+toilet_path=$(command -v toilet)
+[[ -n "${toilet_path}" ]]                                    \
+  || printf "\e[0;1;38;5;196mCannot find 'toilet' in PATH\n" \
+  || exit 1
+
+inputtext="${*}"
+[[ -z "${inputtext}" ]] && inputtext="${USERNAME}"
+[[ -z "${inputtext}" ]] && inputtext="Sample !"
+
+printf "\n\e[0;1;38;5;87m══════════ \e[0;1;38;5;201mPreview\e[0;1;38;5;93m(above) \e[0;1;38;5;87m| \e[0;1;38;5;201mName\e[0;1;38;5;93m(below) \e[0;1;38;5;87m══════════\e[0m\n\n"  && sleep 1
+
+for file in src/*.flf
+do
+  fontname=$(basename --suffix=.flf "${file}")
+    ${toilet_path}    \
+      --gay           \
+      --filter border \
+      --filter crop   \
+      --font "$file"  \
+      --width 2400    \
+      "${inputtext}" 2>/dev/null \
+    && printf "\e[0;1;38;5;201m%s\e[0m\n\n"  "${fontname}"
+done
--- generate-plain.sh
+++ generate-plain.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
+figlet_path=$(command -v figlet)
+[[ -n "${figlet_path}" ]]                                    \
+  || printf "\e[0;1;38;5;196mCannot find 'figlet' in PATH\n" \
+  || exit 1
+
 inputtext="${*}"
 [[ -z "${inputtext}" ]] && inputtext="${USERNAME}"
 [[ -z "${inputtext}" ]] && inputtext="Sample !"
 
+printf "\n══════════ Preview(above) | Name(below) ══════════\n\n" && sleep 1
+
 for file in src/*.flf
 do
-    echo "\n\n════════════════════════════"     "$file"   "════════════════════════════\n\n"
-    figlet -f "$file" -w 2400 "${inputtext}"
+  fontname=$(basename --suffix=.flf "${file}")
+  ${figlet_path} -f "$file" -w 2400 "${inputtext}" \
+    && printf "%s\n\n\n" "${fontname}"
+    # && printf "\n\n══════════ %s ══════════\n\n" "${fontname}"
 done
